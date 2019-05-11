05a4d2f4642e5f8c4bf1cda31a5d6cae1b33f422
archmoj <mojtaba@plot.ly>
2019-05-11 15:57:13 -0400

    add funnelarea trace type

---
+++ src/traces/funnelarea/attributes.js
@@ -0,0 +1,69 @@
+/**
+* Copyright 2012-2019, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+'use strict';
+
+var pieAttrs = require('../pie/attributes');
+var plotAttrs = require('../../plots/attributes');
+var hovertemplateAttrs = require('../../components/fx/hovertemplate_attributes');
+var domainAttrs = require('../../plots/domain').attributes;
+
+var extendFlat = require('../../lib/extend').extendFlat;
+
+module.exports = {
+    labels: pieAttrs.labels,
+    // equivalent of x0 and dx, if label is missing
+    label0: pieAttrs.label0,
+    dlabel: pieAttrs.dlabel,
+    values: pieAttrs.values,
+    marker: pieAttrs.marker,
+    text: pieAttrs.text,
+    hovertext: pieAttrs.hovertext,
+
+    scalegroup: extendFlat({}, pieAttrs.scalegroup, {
+        description: [
+            'If there are multiple funnelareas that should be sized according to',
+            'their totals, link them by providing a non-empty group id here',
+            'shared by every trace in the same group.'
+        ].join(' ')
+    }),
+
+    textinfo: extendFlat({}, pieAttrs.textinfo, {
+        flags: ['label', 'text', 'value', 'percent inital', 'percent total']
+    }),
+
+    hoverinfo: extendFlat({}, plotAttrs.hoverinfo, {
+        flags: ['label', 'text', 'value', 'percent total', 'name'] // TODO: edit to be percent total/initial
+    }),
+
+    hovertemplate: hovertemplateAttrs({}, {
+        keys: ['label', 'color', 'value', 'percent total', 'text'] // TODO: edit to be percent total/initial
+    }),
+
+    textposition: pieAttrs.textposition,
+
+    textfont: pieAttrs.textfont,
+    insidetextfont: pieAttrs.insidetextfont,
+    outsidetextfont: pieAttrs.outsidetextfont,
+
+    title: pieAttrs.title, // TODO: default title.position to 'top center'
+
+    domain: domainAttrs({name: 'funnelarea', trace: true, editType: 'calc'}),
+
+    hole: extendFlat({}, pieAttrs.hole, {
+        description: [
+            'Sets the fraction of the radius to cut out of the funnelarea.',
+            'Use this to make a rectangular shape at the end (funnel openning).'
+        ].join(' ')
+    }),
+
+    sort: pieAttrs.sort,
+    direction: pieAttrs.direction,
+    rotation: pieAttrs.rotation, // TODO: we may remove this?
+    pull: pieAttrs.pull // TODO: we may remove this?
+};
---
+++ src/traces/funnelarea/plot.js
@@ -0,0 +1,15 @@
+/**
+* Copyright 2012-2019, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+'use strict';
+
+var piePlot = require('../pie/plot').plot;
+
+module.exports = function plot(gd, cdModule) {
+    piePlot(gd, cdModule, { type: 'funnelarea' });
+};
